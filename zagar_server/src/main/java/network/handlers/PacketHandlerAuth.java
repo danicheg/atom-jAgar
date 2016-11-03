240d66f589a7a9530752effa0ea7bd098cb805ef
Daniel Esik <e.danicheg@yandex.ru>
2016-11-03 17:13:07 +0300

    Integration our code base from rk #1 to current jAgar server

--- zagar_server/src/main/java/network/handlers/PacketHandlerAuth.java
+++ zagar_server/src/main/java/network/handlers/PacketHandlerAuth.java
@@ -1,6 +1,6 @@
 package network.handlers;
 
-import accountserver.api.AuthenticationServlet;
+import accountserver.entities.token.TokensStorage;
 import main.ApplicationContext;
 import matchmaker.MatchMaker;
 import model.Player;
@@ -17,29 +17,36 @@ import utils.JSONHelper;
 import java.io.IOException;
 
 public class PacketHandlerAuth {
-  public PacketHandlerAuth(@NotNull Session session, @NotNull String json) {
-    CommandAuth commandAuth;
-    try {
-      commandAuth = JSONHelper.fromJSON(json, CommandAuth.class);
-    } catch (JSONDeserializationException e) {
-      e.printStackTrace();
-      return;
+    public PacketHandlerAuth(@NotNull Session session, @NotNull String json) {
+        CommandAuth commandAuth;
+        try {
+            commandAuth = JSONHelper.fromJSON(json, CommandAuth.class);
+        } catch (JSONDeserializationException e) {
+            e.printStackTrace();
+            return;
+        }
+        try {
+            if (!TokensStorage.validateToken(commandAuth.getToken())) {
+                try {
+                    new PacketAuthFail(commandAuth.getLogin(), commandAuth.getToken(),
+                            "Invalid user or password").write(session);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    Player player = new Player(ApplicationContext.instance()
+                            .get(IDGenerator.class).next(), commandAuth.getLogin()
+                    );
+                    ApplicationContext.instance().get(ClientConnections.class).registerConnection(player, session);
+                    new PacketAuthOk().write(session);
+                    ApplicationContext.instance().get(MatchMaker.class).joinGame(player);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    if (!AuthenticationServlet.validateToken(commandAuth.getToken())) {
-      try {
-        new PacketAuthFail(commandAuth.getLogin(), commandAuth.getToken(), "Invalid user or password").write(session);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    } else {
-      try {
-        Player player = new Player(ApplicationContext.instance().get(IDGenerator.class).next(), commandAuth.getLogin());
-        ApplicationContext.instance().get(ClientConnections.class).registerConnection(player, session);
-        new PacketAuthOk().write(session);
-        ApplicationContext.instance().get(MatchMaker.class).joinGame(player);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
 }
