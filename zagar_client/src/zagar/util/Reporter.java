9e6bed3ef46ff54fb64a36f928a95330f537d3a0
Daniel Esik <e.danicheg@yandex.ru>
2016-11-03 15:43:32 +0300

    initial commit

---
+++ zagar_client/src/zagar/util/Reporter.java
@@ -0,0 +1,31 @@
+package zagar.util;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * @author apomosov
+ */
+public class Reporter {
+    public static void reportFail(@NotNull String title, @NotNull String message) {
+        JOptionPane.showMessageDialog(null,
+                message,
+                title,
+                JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void reportWarn(@NotNull String title, @NotNull String message) {
+        JOptionPane.showMessageDialog(null,
+                message,
+                title,
+                JOptionPane.WARNING_MESSAGE);
+    }
+  public static void reportInfo(@NotNull String title, @NotNull String message) {
+      JOptionPane.showMessageDialog(null,
+              message,
+              title,
+              JOptionPane.OK_OPTION);
+  }
+
+}
