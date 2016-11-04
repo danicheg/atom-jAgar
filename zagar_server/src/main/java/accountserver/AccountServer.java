effc2053a01c83ce078a67d699f3b31c43d71e0e
Aleksandr <allxf95@gmail.com>
2016-11-04 05:18:23 +0300

    Add H2 Database Connection

--- zagar_server/src/main/java/accountserver/AccountServer.java
+++ zagar_server/src/main/java/accountserver/AccountServer.java
@@ -4,12 +4,16 @@ import accountserver.auth.AuthenticationFilter;
 import main.Service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.h2.server.web.WebServlet;
 import org.jetbrains.annotations.NotNull;
 
 
 public class AccountServer extends Service {
+
     private final static @NotNull Logger log = LogManager.getLogger(AccountServer.class);
     private final int port;
 
@@ -20,15 +24,18 @@ public class AccountServer extends Service {
     }
 
     private void startApi() {
+
         ServletContextHandler context = new ServletContextHandler();
         context.setContextPath("/");
 
-        org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server(port);
+        Server server = new Server(port);
         server.setHandler(context);
 
-        ServletHolder jerseyServlet = context.addServlet(
-                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-        jerseyServlet.setInitOrder(0);
+        ServletHolder h2Servlet = context.addServlet(WebServlet.class, "/h2/*");
+        h2Servlet.setInitOrder(0);
+
+        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(1);
 
         jerseyServlet.setInitParameter(
                 "jersey.config.server.provider.packages",
@@ -51,4 +58,5 @@ public class AccountServer extends Service {
     public static void main(@NotNull String[] args) throws Exception {
         new AccountServer(8080).startApi();
     }
+
 }
