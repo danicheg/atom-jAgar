957af1839aa609e68842393f0353b77fd281065d
Aleksandr <allxf95@gmail.com>
2016-11-04 06:32:03 +0300

    Register users with database connection completed.

--- zagar_server/src/main/java/entities/user/User.java
+++ zagar_server/src/main/java/entities/user/User.java
@@ -9,7 +9,6 @@ import org.jetbrains.annotations.Nullable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
