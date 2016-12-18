50014e825966321f38e8cc0987b4edf97e4ec227
George <eakarpov@yandex.ru>
2016-12-18 14:02:37 +0300

    Logic of single turn and eating food taken out from hadlerMove to Field

--- zagar_server/src/main/java/model/Field.java
+++ zagar_server/src/main/java/model/Field.java
@@ -1,6 +1,7 @@
 package model;
 
 import com.sun.istack.internal.Nullable;
+import dao.DatabaseAccessLayer;
 import org.eclipse.jetty.util.ConcurrentHashSet;
 import org.jetbrains.annotations.NotNull;
 
@@ -85,6 +86,53 @@ public class Field {
         field.setViruses(viruses);
         field.setFoods(foods);
         return field;
+    }
+
+    public void makeSingleTurn(Player player, float dx, float dy) {
+        for (Cell cell : player.getCells()) {
+            float oldX = cell.getX();
+            float oldY = cell.getY();
+            int newX = Math.round(oldX + (dx - oldX) / cell.getRadius());
+            int newY = Math.round(oldY + (dy - oldY) / cell.getRadius());
+            if (Math.abs(newX) < GameConstants.FIELD_WIDTH && Math.abs(newY) < GameConstants.FIELD_HEIGHT) {
+                cell.setX(newX);
+                cell.setY(newY);
+                Set<Food> foods = player.getSession().getField().getFoods();
+                Location first = new Location(Math.round(oldX), Math.round(oldY));
+                Location second = new Location(Math.round(newX), Math.round(newY));
+                for (Food food : foods) {
+                    float fX = food.getLocation().getX();
+                    float fY = food.getLocation().getY();
+                    Location food_center = new Location(Math.round(fX), Math.round(fY));
+                    if (checkDistance(first, second, food_center, food.getMass(), cell.getMass())) {
+                        cell.setMass(cell.getMass() + food.getMass());
+                        player.getSession().getField().getFoods().remove(food);
+                        player.getUser().setScore(player.getScore());
+                        DatabaseAccessLayer.updateUser(player.getUser());
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean checkDistance(Location first, Location second, Location foodCenter,
+                                  float foodLength, float cellLength) {
+        Vector vector = new Vector(second.getX() - first.getX(), second.getY() - first.getY());
+        Vector normalVector = vector.makeNormal().normalize();
+        Vector foodNormalVector = normalVector.extend(foodLength);
+        Vector cellNormalVector = normalVector.extend(cellLength);
+
+        Location edgeUp = foodNormalVector.getEnd(foodCenter);
+        Location edgeDown = foodNormalVector.getStart(foodCenter);
+
+        Location centerCellGone = cellNormalVector.intersectWith(vector, first, foodCenter);
+        Location edgeUpCell = cellNormalVector.getEnd(centerCellGone);
+        Location edgeDownCell = cellNormalVector.getStart(centerCellGone);
+
+        float distanceOne = edgeDown.distanceTo(edgeUpCell);
+        float distanceTwo = edgeUp.distanceTo(edgeDownCell);
+        float length = 2 * cellNormalVector.length();
+        return ((distanceOne < length) && (distanceTwo < length));
     }
 
 }
